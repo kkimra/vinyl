22dc1c25d8d64683400d5ff7a4f8d8e181a0f517
Sergey <kkimra@yandex.ru>
2017-07-25 22:28:44 +0400

    css fix

--- resources/views/singlevinyl.blade.php
+++ resources/views/singlevinyl.blade.php
@@ -9,13 +9,16 @@
 @endsection
 
 @section('content')
-<div class="vinyl">
-      <article class="vinyl-single">
-      <h2>{{$vinyl->name}}</h2>
-      <img src="{{URL::asset('uploads/' . $vinyl->image)}}" />
-      <br>
-      <span class="info">{{$authors->author_id}}|{{$vinyl->created_at}}</span>
-      <h4>{!!$vinyl->tracklist!!}</h4>
-      </article>
+<div class="container">
+  <div class="vinyl">
+        <article class="vinyl-single">
+        <h2>{{$vinyl->name}}</h2>
+        <img src="{{URL::asset('uploads/' . $vinyl->image)}}" />
+        <br>
+        <span class="info">{{$authors->author_id}}|{{$vinyl->created_at}}</span>
+        {!!$vinyl->tracklist!!}
+        </article>
+  </div>
 </div>
+
 @endsection
